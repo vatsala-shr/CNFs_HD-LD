e73dc09ccf2fdae5872dbc106d59f5f6462e348d
Vinayak <vbhartia02@gmail.com>
2023-06-08 12:41:36 +0530

    Minor changes!

--- script.py
+++ script.py
@@ -1,5 +1,5 @@
 import subprocess
-shape = [0.5, 1.0]
+shape = [0.5, 0.75]
 noise_iter = [1, 2, 4, 8, 16]
 for j in range(len(noise_iter)):
     for i in range(len(shape)):
--- script1.py
+++ script1.py
@@ -3,5 +3,5 @@ shape = [1.0, 1.5, 2]
 noise_iter = [1, 2, 4, 8, 16]
 for j in range(len(noise_iter)):
     for i in range(len(shape)):
-        command = f'python3 script_train.py --sup_ratio 1 --gpu_id 4 --crap_ratio 0.5 --shape {shape[i]} --noise_iter {noise_iter[j]} --noise True'
+        command = f'python3 script_train.py --sup_ratio 1 --gpu_id 5 --crap_ratio 0.5 --shape {shape[i]} --noise_iter {noise_iter[j]} --noise True'
         subprocess.run(command, shell = True)
